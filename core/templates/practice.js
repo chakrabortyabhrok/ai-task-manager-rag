73cee36633620ed5221f4565ee0505a08766c4b2
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-08-18 13:06:09 +0530

    commit test

--- core/templates/practice.js
+++ core/templates/practice.js
@@ -1,8 +1,30 @@
-//let a = 22
-let b = "My name is"
+let p1 = new Promise((resolve, reject)=>{
+    console.log('Promise is pending')
+    setTimeout(()=>{
+        console.log('I am a promise and I am fulfiled')
+        resolve(true)
+    }, 3000)
+})
 
-const item = {
-    "name": "abhrok",
-    "age": 19
-}
-console.log("My name is",item["name"], "and my age is", item["age"])
+
+console.log(p1)
+
+
+let p2 = new Promise((resolve, reject)=>{
+    console.log('Promise is pending')
+    setTimeout(()=>{
+        console.log('I am a promise and I am rejected')
+        reject(true)
+    }, 3000)
+})
+
+
+p2.then((value) => {
+    console.log(value)
+},(error) => {
+    console.log(typeof error)
+})
+
+p2.catch(() => {
+    console.log('there is some error')
+})
