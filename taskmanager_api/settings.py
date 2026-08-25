ca304c4d0f794763a3bc134d01d9454390422881
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-08-25 13:39:50 +0530

    connecting deployed frontend and backend - TEST 1

--- taskmanager_api/settings.py
+++ taskmanager_api/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://taskmanager-frontend-chi-one.vercel.app",
 ]
 
 ROOT_URLCONF = 'taskmanager_api.urls'
